5908ee4e3bd5d92f4f1aee739c3032f4387dd4fe
Pedro Huenchullán <phuenchullan.diaz@gmail.com>
2019-05-17 00:14:53 -0400

    Configuracion del localStorage, Fontawesome y mas

--- src/app/components/estudiantes/estudiantes.component.ts
+++ src/app/components/estudiantes/estudiantes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Estudiante } from '../../interfaces/estudiante';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-estudiantes',
@@ -11,21 +12,52 @@ export class EstudiantesComponent implements OnInit {
 
   private estudiante:Estudiante;
   private forma: FormGroup;
+  private estudiantes:Array<Estudiante> = [];
 
   constructor() {
     this.forma = new FormGroup({
-      rut: new FormControl('19760338'),
-      dv: new FormControl('9'),
-      name: new FormControl('Alejandro'),
-      lastName: new FormControl('Perez'),
-      phone: new FormControl('58906145'),
-      carrera: new FormControl('Ingeniería en Informática'),
-      email: new FormControl('fjklasd@gmail.com'),
-      promocion: new FormControl('2017')
+      rut: new FormControl(''),
+      dv: new FormControl(''),
+      name: new FormControl(''),
+      lastName: new FormControl(''),
+      phone: new FormControl(''),
+      carrera: new FormControl(''),
+      email: new FormControl(''),
+      promocion: new FormControl('')
+    });
+    this.obtenerEstudiantes().subscribe(data => {
+      this.estudiantes = data;
+    })
+  }
+
+  ngOnInit() { }
+
+  obtenerEstudiantes():Observable<any>{
+    return new Observable((observer) => {
+      observer.next(
+        JSON.parse(localStorage.getItem('estudiantes'))
+      )
     });
   }
 
-  ngOnInit() {
+  generateData(){
+    this.estudiante = this.forma.value;
+    console.log(this.estudiante);
+    if (this.estudiante != null) {
+      Array<Estudiante>(this.estudiantes.push(this.estudiante));
+      localStorage.setItem('estudiantes',JSON.stringify(this.estudiantes));
+    }
+  }
+
+  deleteEstudiante(i){
+    console.log('Elemento que quiere eliminar es '+i);
+    this.estudiantes.splice(i,1);
+    localStorage.setItem('estudiantes',JSON.stringify(this.estudiantes));
+    console.log(this.estudiantes);
+  }
+
+  editarEstudiante(i){
+    console.log('Editando estudiante '+i);
   }
 
 }
